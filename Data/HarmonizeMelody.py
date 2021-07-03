7ee0051c8d06b71e5ded4e57941a73663fb95302
arunmghosh <86308827+arunmghosh@users.noreply.github.com>
2021-07-03 18:00:03 -0400

    Update HarmonizeMelody.py

--- Data/HarmonizeMelody.py
+++ Data/HarmonizeMelody.py
@@ -25,13 +25,22 @@ def get_notes(instrument_type, path):
 def getChordPalette(symbols):
 	chords = list()
 	for s in symbols:
-		chords.append(Chord.chordFromSymbol, s, 0)
-		chords.append(Chord.chordFromSymbol, s, 1)
-		chords.append(Chord.chordFromSymbol, s, 2)
+		chords.append(Chord.chordFromSymbol(s, 0))
+		chords.append(Chord.chordFromSymbol(s, 1))
+		chords.append(Chord.chordFromSymbol(s, 2))
 	return chords
 
 def getFirstChord(note, palette):
-	
+	options = dict{} #this will be fed to the scoring algorithm when it has been fully populated
+	for chord in palette:
+		options[chord] = dict{} #populate second-level dictionaries and beyond in getChord method
+	return options
+
+def getChord(note, options): #different case than the first chord because more constraints come into play
+	#iterates through dictionary and populates the empty dictionaries stored under keys that represent the options for the previous chord
+	#the options parameter could be the high-level dictionary defined in getFirstChord or a lower-level dictionary
+	#call getSuccessors() and checkForParallels(), each empty dictionary is populated with the successors that pass the parallel check
+	#the way I'm imagining this algorithm working right now uses recursion, but I'm trying to think of a way to avoid that
 
 def main():
 	# retrieve MIDI file
