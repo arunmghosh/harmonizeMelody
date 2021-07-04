c6a01152add31490e3101829d11924d9329f424c
arunmghosh <86308827+arunmghosh@users.noreply.github.com>
2021-07-04 11:02:39 -0400

    Update HarmonizeMelody.py

--- Data/HarmonizeMelody.py
+++ Data/HarmonizeMelody.py
@@ -33,14 +33,28 @@ def getChordPalette(symbols):
 def getFirstChord(note, palette):
 	options = dict{} #this will be fed to the scoring algorithm when it has been fully populated
 	for chord in palette:
-		options[chord] = dict{} #populate second-level dictionaries and beyond in getChord method
+		options[chord] = dict{} 
 	return options
 
-def getChord(note, options): #different case than the first chord because more constraints come into play
+def getNextChord(note, next_note, chord): #different case than the first chord because more constraints come into play
 	#iterates through dictionary and populates the empty dictionaries stored under keys that represent the options for the previous chord
 	#the options parameter could be the high-level dictionary defined in getFirstChord or a lower-level dictionary
 	#call getSuccessors() and checkForParallels(), each empty dictionary is populated with the successors that pass the parallel check
-	#the way I'm imagining this algorithm working right now uses recursion, but I'm trying to think of a way to avoid that
+	choices = dict{}
+	for c in chord.getSuccesors(next_note):
+		if not chord.hasParallels(c, note, next_note):
+			choices[c] = dict{} 
+	return choices 
+
+def getChordProgressions(melody, symbols):
+	#define palette
+	palette = getChordPalette(symbols)
+	
+	#start by determining which chords can start the progression, as the rules get more strict for the successors
+	firstNote = melody[0]
+	progressions = getFirstChord(firstNote, palette) #high-level structure, this will be returned (this is a dictionary)
+	
+	#assign values to keys (nesting dictionaries)		
 
 def main():
 	# retrieve MIDI file
